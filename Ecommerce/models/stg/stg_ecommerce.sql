9386d80379060a05fe9d3cbf9bac939c6fc563b9
dpires <dpiresmartins@yahoo.com.br>
2022-07-09 18:22:55 +0100

    Created first view with minimum changes and deleted folder examples

---
+++ Ecommerce/models/stg/stg_ecommerce.sql
@@ -0,0 +1,24 @@
+-- Configs if necessity
+-- Change names of some columns and with minimum transformations
+WITH stg_ecommerce AS (
+    SELECT
+        *
+    FROM
+        ecommerce.raw.ecommerce_raw
+)
+SELECT
+    id AS id_row,
+    warehouse_block,
+    mode_of_shipment,
+    customer_care_calls,
+    customer_rating,
+    cost_of_the_product AS cost_product,
+    prior_purchases,
+    INITCAP (
+        product_importance
+    ) AS product_importance,
+    gender,
+    discount_offered,
+    weight_in_gms
+FROM
+    stg_ecommerce
