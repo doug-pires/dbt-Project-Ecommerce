f7c610e92238c867c57f9f1891fffed76bee3fb4
dpires <dpiresmartins@yahoo.com.br>
2022-07-16 18:52:11 +0100

    Dim date completed

--- Ecommerce/models/stg/stg_date.sql
+++ Ecommerce/models/stg/stg_date.sql
@@ -1,8 +1,9 @@
+-- Config
 {{ config(
     materialized = 'view'
 ) }}
 {{ dbt_utils.date_spine(
     datepart = "day",
-    start_date = "cast('2019-01-01' as date)",
+    start_date = "cast('2018-01-01' as date)",
     end_date = "cast('2020-01-01' as date)"
 ) }}
