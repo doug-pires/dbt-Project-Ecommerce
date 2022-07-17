25b6b6c2de721ba854798995a536aaa1825dc96b
dpires <dpiresmartins@yahoo.com.br>
2022-07-17 21:42:56 +0100

    I got to dynamize the start date and year date, so for each project we can choose start_date and end_date for our dim_date

--- Ecommerce/models/stg/stg_date.sql
+++ Ecommerce/models/stg/stg_date.sql
@@ -2,8 +2,9 @@
 {{ config(
     materialized = 'view'
 ) }}
+---------------------------------
 {{ dbt_utils.date_spine(
     datepart = "day",
-    start_date = "cast('2018-01-01' as date)",
-    end_date = "cast('2020-01-01' as date)"
+    start_date = "cast(" ~ "'" ~ var('year_min') ~ "-01-01' " ~ "as date)",
+    end_date = "cast(" ~ "'" ~ var('year_max') ~ "-12-31' " ~ "as date)"
 ) }}
